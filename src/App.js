1ff50f794cfbac70848be38c8ce7d5168f7385d5
HendrikBD <hendrik.danen@gmail.com>
2018-07-17 11:30:54 -0400

    Importing store to app and passing via Provider (+ importing app styling here instead)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
+import './app.css';
 import { Notes } from './components/notes.js';
 import { Navbar } from './components/navbar.js';
+import store from './store';
+
 
 export default class App extends React.Component {
   render () {
     return (
-      <div>
-        <Navbar/>
-        <Notes/>
-      </div>
+      <Provider store={store}>
+        <div>
+          <Navbar/>
+          <Notes/>
+        </div>
+      </Provider>
     )
   }
 }
