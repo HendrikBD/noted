6f93dd0659d27c4777cfc7946ba2c0b5b69cefda
HendrikBD <hendrik.danen@gmail.com>
2018-07-17 12:00:58 -0400

    Trying out marked package and ensuring html is sanitized

--- src/components/notes.js
+++ src/components/notes.js
@@ -2,13 +2,20 @@ import React from 'react';
 import css from './style/notes.css'
 import { Sidebar } from './sidebar'
 
+var marked = require("marked");
+marked.setOptions({sanitize: true})
+
 export class Notes extends React.Component {
 
   render() {
+
+    let mark = marked('# Hello There!\n<script>console.log("from the other side...")</script>')
+
     return (
       <div id="notes">
         <Sidebar/>
         <div class="note">
+          <div dangerouslySetInnerHTML={{__html: mark}}></div>
         </div>
       </div>
     )
