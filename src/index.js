78c9c34009b74ef66a31c20ef94a6b3a3045b935
HendrikBD <hendrik.danen@gmail.com>
2018-07-17 10:01:07 -0400

    Importing App xml, changing html app id to root and changing render import

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import css from './app.css';
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
-import { Notes } from './components/notes.js';
-import { Navbar } from './components/navbar.js';
+import App from './App.js';
 
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        <Navbar/>
-        <Notes/>
-      </div>
-    )
-  }
-}
-
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('root'));
