df12dc4e453bd3b7efdb1837d4da253dcd96f683
HendrikBD <hendrik.danen@gmail.com>
2018-07-17 20:23:37 -0400

    Adding some structure for sidebar and embedding hierarchy component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Hierarchy from './hierarchy';
 
 const Sidebar = () => (
-  <div className='sidebar'></div>
+  <div className='sidebar'>
+    <div className='disp'>
+      <Hierarchy/>
+    </div>
+    <div className='buttons'></div>
+  </div>
 )
 
 export { Sidebar }
