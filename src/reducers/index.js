cf2a36b56585b3c6b8c4ea9648bf55b79c02789d
HendrikBD <hendrik.danen@gmail.com>
2018-07-17 20:26:25 -0400

    Importing initial state from library and importing notedReducer through default export

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { notedReducer } from './notedReducer';
+import notedReducer from './notedReducer';
 
 export default combineReducers({
   noted: notedReducer
