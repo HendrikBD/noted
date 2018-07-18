fb96e1f56536013cb11293f6bca831eec4c1759f
HendrikBD <hendrik.danen@gmail.com>
2018-07-18 18:56:36 -0400

    Re-organizing node to better suit trace

--- src/components/node.js
+++ src/components/node.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import TraceBlock from './traceBlock';
 
 
 export default class Node extends React.Component {
   render() {
     return (
       <div className="node">
-        <svg>
-          <circle cx='9' cy='9' r='7.5' stroke-width='3' stroke='black' fill="none"></circle>
-          <circle cx='9' cy='9' r='4.5' stroke-width='1' fill="black"></circle>
-        </svg>
-        {this.props.nodes.byId[this.props.nodeId].name}
-        { this.props.nodes.byId[this.props.nodeId].childNodes.map(nodeId => 
-          <Node nodes={this.props.nodes} nodeId={nodeId}/>
-        )}
+
+        <div className="head">
+          <div className='icon'>
+            <svg>
+              <circle cx='9' cy='9' r='7.5' stroke-width='3' stroke='black' fill="none"></circle>
+              <circle cx='9' cy='9' r='4.5' stroke-width='1' fill="black"></circle>
+            </svg>
+          </div>
+        </div>
+        <div className="title">
+          {this.props.nodes.byId[this.props.nodeId].name}
+        </div>
+
+        <div className="trace">
+        </div>
+
+        <div className="children">
+          { this.props.nodes.byId[this.props.nodeId].childNodes.map(nodeId =>
+            <Node nodes={this.props.nodes} nodeId={nodeId}/>
+          )}
+        </div>
+
       </div>
     )
   }
