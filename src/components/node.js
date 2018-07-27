6cb4008ff5846a60cfff1e8f77da41567710880f
HendrikBD <hendrik.danen@gmail.com>
2018-07-27 10:16:06 -0400

    Adding basic toggle functionality to node

--- src/components/node.js
+++ src/components/node.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { toggleNode } from '../actions/notedActions';
+
 import TraceBlock from './traceBlock';
 
 
 export default class Node extends React.Component {
+
+  onClick(){
+    this.props.toggleNode(this.props.nodeId)
+  }
+
+
   render() {
+    if(this.props.nodes.byId[this.props.nodeId].toggled){
+      childNodes=[];
+      this.props.nodes.byId[this.props.nodeId].childNodes.map(nodeId=>
+        childNodes.push(<Node nodes={this.props.nodes} nodeId={nodeId} toggleNode={this.props.toggleNode}/>)
+      )
+    }
+
     return (
       <div className="node">
 
         <div className="head">
-          <div className='icon'>
+          <div className='icon' onClick={this.onClick.bind(this)}>
             <svg>
               <circle cx='9' cy='9' r='7.5' stroke-width='3' stroke='black' fill="none"></circle>
               <circle cx='9' cy='9' r='4.5' stroke-width='1' fill="black"></circle>
