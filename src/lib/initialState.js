250f72ad89381055cd37ae840186b874bf00be8a
HendrikBD <hendrik.danen@gmail.com>
2018-07-27 11:40:46 -0400

    Adding trace heights to state, to be calculated later

--- src/lib/initialState.js
+++ src/lib/initialState.js
@@ -6,70 +6,71 @@ const initialState = {
           name: 'root',
           parentNode: null,
           childNodes: [1, 6],
+          traceHeights: [0,0],
         },
         1: {
           id: 1,
           name: 'Software',
           parentNode: 0,
           childNodes: [2,5],
+          traceHeights: [0,0],
           toggled: true,
-          traceHeight: 0
         },
         2: {
           id: 2,
           name: 'Projects',
           parentNode: 1,
           childNodes: [3, 4],
+          traceHeights: [0,0],
           toggled: true,
-          traceHeight: 0
         },
         3: {
           id: 3,
           name: 'Hopper',
           parentNode: 2,
           childNodes: [],
+          traceHeights: [],
           toggled: false,
-          traceHeight: 0
         },
         4: {
           id: 4,
           name: 'Noted',
           parentNode: 2,
           childNodes: [],
+          traceHeights: [],
           toggled: false,
-          traceHeight: 0
         },
         5: {
           id: 5,
           name: 'Python',
           parentNode: 1,
           childNodes: [],
+          traceHeights: [],
           toggled: false,
-          traceHeight: 0
         },
         6: {
           id: 6,
           name: 'Books',
           parentNode: 0,
           childNodes: [7,8],
+          traceHeights: [0,0],
           toggled: true,
-          traceHeight: 0
         },
         7: {
           id: 7,
           name: '1984',
           parentNode: 6,
           childNodes: [],
+          traceHeights: [],
           toggled: false,
-          traceHeight: 0
         },
         8: {
           id: 8,
           name: 'Brave New World',
           parentNode: 6,
           childNodes: [],
+          traceHeights: [],
           toggled: false,
-          traceHeight: 0
         }
       },
       allIds: [0,1,2,3,4,5,6,7,8]
