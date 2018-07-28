ab2fdf3bb6e9291f567517429e1944c45e26acd0
HendrikBD <hendrik.danen@gmail.com>
2018-07-28 14:00:56 -0400

    Adding function to draw trace

--- src/components/traceBlock.js
+++ src/components/traceBlock.js
@@ -13,6 +13,21 @@ export default class Node extends React.Component {
     return height
   }
 
+  getTraceSVG() {
+    let maxTrace = Math.max(0, ...this.props.traceHeights);
+
+    let trace = [
+      <path d={"M 10,0 v " + maxTrace} stroke-width="8" stroke="black"/>,
+      <path d={"M 10," + maxTrace + " a 4 4 0 0 0 4 4"} stroke-width="8" stroke="black"/>
+    ]
+
+    this.props.traceHeights.forEach(height => {
+      trace.push(<path d={"M 14," + Number(height+4) + "h 25"} stroke-width="8" stroke="black"/>)
+    })
+
+    return trace
+  }
+
   render() {
 
     let points = "10,0 10," + this.props.nodes.byId[this.props.nodeId].traceHeight;
