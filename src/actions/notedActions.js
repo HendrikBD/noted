0f15a2561aa1e296e95b9b2d623c0615dee9ae3b
HendrikBD <hendrik.danen@gmail.com>
2018-07-31 14:25:50 -0400

    Adding action function for changing trace variables

--- src/actions/notedActions.js
+++ src/actions/notedActions.js
@@ -1,8 +1,16 @@
-import { TOGGLE_NODE } from './types';
+import { TOGGLE_NODE, UPDATE_TRACE, UPDATE_TRACE_HEIGHT, UPDATE_CHILD_HEIGHTS, UPDATE_TRACE_WIDTHS } from './types';
 
 export function toggleNode(nodeId) {
   return {
     type: TOGGLE_NODE,
     nodeId: nodeId
+  }
+}
+
+export function updateTrace(nodeId, traceUpdate) {
+  return {
+    type: UPDATE_TRACE,
+    nodeId: nodeId,
+    traceUpdate: traceUpdate
   }
 }
