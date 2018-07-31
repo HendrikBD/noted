9bb56d554407f94b61838b084379beb788fccf8f
HendrikBD <hendrik.danen@gmail.com>
2018-07-31 14:26:56 -0400

    Adding reducer functionality for changing trace state

--- src/reducers/notedReducer.js
+++ src/reducers/notedReducer.js
@@ -1,11 +1,17 @@
-import { TOGGLE_NODE } from '../actions/types';
+import { TOGGLE_NODE, UPDATE_TRACE, UPDATE_TRACE_HEIGHT, UPDATE_CHILD_HEIGHTS, UPDATE_TRACE_WIDTHS } from '../actions/types';
 import initialState from '../lib/initialState';
 
 export default function(state = initialState, action) {
+  const newState = Object.assign({}, state);
+
   switch(action.type) {
     case TOGGLE_NODE:
-      const newState = Object.assign({}, state);
       newState.nodes.byId[action.nodeId].toggled = !newState.nodes.byId[action.nodeId].toggled;
+      return newState;
+    case UPDATE_TRACE:
+      Object.keys(action.traceUpdate).forEach(key => {
+        newState.nodes.byId[action.nodeId][key] = action.traceUpdate[key];
+      })
       return newState;
     default:
       return state;
