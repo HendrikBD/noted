9be43ed6ff74f9111f0a9b0f0fe2c635dd4fa504
HendrikBD <hendrik.danen@gmail.com>
2018-08-01 15:38:09 -0400

    Including child nodes of untoggled nodes to allow for a transition when adding/removing

--- src/components/node.js
+++ src/components/node.js
@@ -28,16 +28,17 @@ class Node extends React.Component {
 
   render() {
     var childNodes=[];
+    var nodeClasses = (this.props.nodes.byId[this.props.nodes.byId[this.props.nodeId].parentNode].toggled) ? 'node toggled': 'node';
     let traceOn = (this.props.nodes.byId[this.props.nodeId].childNodes.length>0 && this.props.nodes.byId[this.props.nodeId].toggled)
 
-    if(this.props.nodes.byId[this.props.nodeId].toggled){
-      this.props.nodes.byId[this.props.nodeId].childNodes.map(nodeId=>
-        childNodes.push(<Node key={nodeId} nodes={this.props.nodes} nodeId={nodeId} toggleNode={this.props.toggleNode} updateTrace={this.props.updateTrace} updateParentTrace={this.updateParentTrace.bind(this)}/>)
-      )
-    }
+    this.props.nodes.byId[this.props.nodeId].childNodes.forEach(nodeId => {
+      if(this.props.nodes.byId[this.props.nodes.byId[this.props.nodes.byId[nodeId].parentNode].parentNode].toggled){
+        childNodes.push(<Node key={nodeId} nodes={this.props.nodes} nodeId={nodeId} toggleNode={this.props.toggleNode} updateTrace={this.props.updateTrace} updateParentTrace={this.updateParentTrace.bind(this)} />)
+      }
+    })
 
     return (
-      <div className="node">
+      <div className={nodeClasses}>
 
         <div className="head">
           <div className='icon' onClick={this.onClick.bind(this)}>
