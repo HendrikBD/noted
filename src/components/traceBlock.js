037fc430ee185ad4e40456bed5b46fe4cf9046fc
HendrikBD <hendrik.danen@gmail.com>
2018-08-01 15:46:41 -0400

    Moving functionality of calculating trace heights and updating trace into traceblock component

--- src/components/traceBlock.js
+++ src/components/traceBlock.js
@@ -28,6 +28,35 @@ export default class Node extends React.Component {
     return trace
   }
 
+  // Continually updates the trace height request animation frame. Decides whether vertical lines or horizontal lines should be changed. Will update the trace heights as well as all trace widths.
+  updateTrace() {
+    let node = this.props.nodes.byId[this.props.nodeId];
+    let maxTrace = Math.max(0, ...this.props.nodes.byId[this.props.nodeId].childHeights);
+    let traceDiff = (maxTrace - this.props.nodes.byId[this.props.nodeId].traceHeight);
+
+    this.props.updateTrace(this.props.nodeId, {traceHeight: node.traceHeight+2*Math.sign(traceDiff)});
+
+    // If the last childs width and the trace height are equal to the target values. When closing, target values are zero, when opening target values are a static width and the max trace
+    if(this.props.nodes.byId[this.props.nodeId].traceHeight<maxTrace){
+      window.requestAnimationFrame(this.updateTrace.bind(this))
+    }
+  }
+
+  // Calculates the heights of each child. If toggled off will return empty array. Should be called every time this node is, or any of its children is toggled. (Not when parent toggled)
+  updateChildHeights() {
+    let childHeights = [];
+    if(this.props.nodes.byId[this.props.nodeId].toggled) {
+      childHeights.push(16)
+      for(let i=0; i<this.props.nodes.byId[this.props.nodeId].childNodes.length-1; i++){
+        let child = this.props.nodes.byId[this.props.nodeId].childNodes[i];
+        let nestedChildren = countNestedChildren(this.props.nodes, child);
+        childHeights.push(28*(nestedChildren+1) + childHeights[i])
+      }
+    }
+    this.props.updateTrace(this.props.nodeId, {childHeights: childHeights})
+    this.updateTrace();
+  }
+
   render() {
     let traceBlockHeight;
     if(this.props.nodes.byId[this.props.nodeId].toggled) {
@@ -46,4 +75,16 @@ export default class Node extends React.Component {
       </div>
     )
   }
+}
+
+function countNestedChildren(nodes, nodeId) {
+  let nestedChildren = 0;
+
+  if(nodes.byId[nodeId].childNodes.length>0 && nodes.byId[nodeId].toggled){
+    nodes.byId[nodeId].childNodes.forEach((child) => {
+      nestedChildren++;
+      nestedChildren += countNestedChildren(nodes, child);
+    })
+  }
+  return nestedChildren;
 }
