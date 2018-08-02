f0f78547c11de05d2dd6cc32cbac305350763d84
HendrikBD <hendrik.danen@gmail.com>
2018-08-02 19:49:44 -0400

    Changing traceblock to correct class name

--- src/components/traceBlock2.js
+++ src/components/traceBlock2.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Node extends React.Component {
+export default class TraceBlock extends React.Component {
   constructor(props){
     super(props);
     this.updateChildHeights();
