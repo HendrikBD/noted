ba51eeadde7907921f97ad9a874047588ef1a84c
HendrikBD <hendrik.danen@gmail.com>
2018-08-07 12:26:31 -0400

    Changing function order to ensure maxHeight is updated before updating trace

--- src/components/node2.js
+++ src/components/node2.js
@@ -21,8 +21,8 @@ class Node extends React.Component {
     if(this.props.nodes.byId[this.props.nodeId].parentNode>0) {
       this.props.updateParentTraces();
     }
-    this.trace.current.updateTrace();
     this.props.updateTraceState(this.props.nodeId, {maxHeight: 28*(this.countNestedChildren.bind(this)(this.props.nodes, this.props.nodeId))})
+    this.trace.current.updateTrace();
 
   }
 
